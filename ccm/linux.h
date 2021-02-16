178b638b6eacccc6ac36a1783917cf20fb2b78c1
thekotekjournal <63573571+thekotekjournal@users.noreply.github.com>
2021-02-16 09:49:35 +0700

    Update
    Make Comment

--- ccm/linux.h
+++ ccm/linux.h
@@ -1,2 +1,9 @@
-void input(std::string&, std::string);
-void ExecutableRun(std::string&);
+// If You get some error in this sory
+// this missing librart #include <string>
+// but this not error in compile
+
+// Input Library from CCM
+void input(std::string &, std::string);
+
+// Run Shell And Get output from refrench string
+void ExecutableRun(std::string &);
