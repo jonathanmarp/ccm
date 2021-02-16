2c2901f97a113d4d86420b3adc02973a17b0b2e9
thekotekjournal <63573571+thekotekjournal@users.noreply.github.com>
2021-02-16 07:24:32 +0700

    Updating
    Change so that fewer code will be seen but I immediately do pake mobile phone then from that rather disconnected work.

--- ccm.h
+++ ccm.h
@@ -6,22 +6,7 @@
 
         #ifdef _WIN32
 
-            #ifdef __cplusplus
-                extern "C" {
-            #endif
-
-                #ifdef BUILD_SHARED_LIB
-                    #define CCM __declspec(dllexport)
-                #else
-                    #define CCM __declspec(dllimport)
-                #endif
-
-                void CCM input(std::string&, std::string);
-                void CCM ExecutableRun(std::string&);
-
-            #ifdef __cplusplus
-                }
-            #endif
+            #include "ccm/windows.h"
 
         #else
             void input(std::string&, std::string);
