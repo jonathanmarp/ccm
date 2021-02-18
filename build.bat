ea241ba7cfa27a79912d0b213043bfbbd596857e
thekotekjournal <63573571+thekotekjournal@users.noreply.github.com>
2021-02-18 21:47:58 +0700

    Delete build.bat

--- build.bat
+++
@@ -1,1 +0,0 @@
-g++ -ffast-math -fstack-arrays -fmax-stack-var-size=100000 -fno-protect-parens -foptimize-strlen -foptimize-sibling-calls -shared -Wall -fPIC -DBUILD_SHARED_LIB ccm.cpp ls.cpp clear.cpp pwd.cpp exec1.cpp exec2.cpp exit.cpp color.cpp cd.cpp -o ccm.dll -Wl,--out-implib,libccm.a
